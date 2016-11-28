6c749d68bdc49ad2290430d193264672a1dfec1f
pokohide <hyde31415@gmail.com>
2016-11-29 02:22:30 +0900

    雛形生成

---
+++ server/db/mysql.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+    "github.com/Sirupsen/logrus"
+    "github.com/OthloTech/OthloCalendar/server/conf"
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/gocraft/dbr"
+)
+
+func Init() *dbr.Session {
+    session := getSession()
+    return session
+}
+
+func getSession() *dbr.Session {
+    db, err := dbr.Open("mysql",
+        conf.USER+":"+conf.PASSWORD+"@tcp("+conf.HOST+":"+conf.PORT+")/"+conf.DB,
+        nil)
+
+    if err != nil {
+        logrus.Error(err)
+    } else {
+        session := db.NewSession(nil)
+        return session
+    }
+    return nil
+}
