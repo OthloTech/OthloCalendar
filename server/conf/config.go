9e4180bd1d4b234df1006c1977f6f3f836a95012
pokohide <hyde31415@gmail.com>
2016-11-29 04:11:57 +0900

    また今度やるよ..

---
+++ server/conf/config.go
@@ -0,0 +1,16 @@
+package conf
+
+const (
+    
+)
+
+
+package conf
+
+const (
+	USER     string = "root"
+	PASSWORD string = "mysql01"
+	DB       string = "sample"
+	HOST     string = "192.168.99.100"
+	PORT     string = "32769"
+)
