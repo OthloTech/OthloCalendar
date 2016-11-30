ef7d01a00b538b43ae730973a43e207878a66511
pokohide <hyde31415@gmail.com>
2016-11-30 16:37:51 +0900

    うまくパースできない

--- test.go
+++ test.go
@@ -6,28 +6,54 @@ import (
 	"io/ioutil"
 	"net/http"
 	"time"
+	//"strings"
 )
 
 const (
 	connpassEndpoint = "https://connpass.com/api/v1/event/"
+	atndEndpoint = "http://api.atnd.org/events/"
 )
 
 func main() {
 	now := time.Now()
 	ym := fmt.Sprintf("%v%v", now.Year(), int(now.Month()))
 
-	events := connpass("python", ym)
+	// events, err := connpass("python", ym)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// for i, event := range events {
+ //  		fmt.Print(fmt.Sprintf("index:%d,value:%s\n", i, event.Title))
+	// }
+
+	events, err := atnd("python", ym)
+	if err != nil {
+		fmt.Println(err)
+	}
 	for i, event := range events {
   		fmt.Print(fmt.Sprintf("index:%d,value:%s\n", i, event.Title))
-	}
+	}	
 }
 
-func connpass(keyword, ym string) []Events {
+/*
+  connpassの使用できるparam See: https://connpass.com/about/api/
+  event_id int 
+  keyword string
+  keyword_or string
+  ym string
+  nickname string 参加者のニックネーム
+  owner_nickname string 管理者のニックネーム
+  series_id int
+  start int
+  order int
+  count int
+  format string default json
+*/
+func connpass(keyword, ym string) ([]Events, error) {
 	url := fmt.Sprintf("%s?keyword=%s&ym=%s&count=10", connpassEndpoint, keyword, ym)
 	res, err := http.Get(url)
 	if err != nil {
-		fmt.Println(err)
-		return nil
+		return nil, err
 	}
 	defer res.Body.Close()
 
@@ -36,44 +62,74 @@ func connpass(keyword, ym string) []Events {
 
 	err = json.Unmarshal(body, &eventData)
 	if err != nil {
-		fmt.Println(err)
-		return nil
+		return nil, err
 	}
 	fmt.Println(eventData.ResultsReturned)
-	return eventData.Events
+	return eventData.Events, nil
+}
+
+func atnd(keyword, ym string) ([]Events, error) {
+	url := fmt.Sprintf("%s?keyword=%s&ym=%s&count=10", atndEndpoint, keyword, ym)
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	var eventData EventData
+
+	//escapedBody := []byte(strings.Replace(body, "'", "\\'", -1))
+
+	err = json.Unmarshal(body, &eventData)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	fmt.Println(eventData.ResultsReturned)
+	return eventData.Events, nil
 }
 
 type EventData struct {
-	ResultsReturned int
-	Events []Events
-	ResultsStart int
-	ResultsAvailable int
+	ResultsReturned int `json:"results_returned"`
+	ResultsStart int `json:"results_start"`
+	ResultsAvailable int `json:"results_available"`
+	Events []Events `json:"events"`
 }
 
 type Events struct {
-	EventURL string
-	EventType string
-	OwnerNickname string
-	Series Series
-	UpdatedAt time.Time
-	Lat string
-	StartedAt time.Time
-	HashTag string
-	Title string
-	EventId int
-	Lon string
-	Waiting int
-	Limit int
-	OwnerID int
-	OwnerDisplayName string
-	Description string
-	Address string
-	EndedAt time.Time
-	Place string
+	EventId int `json:"event_id"`
+	EventURL string `json:"event_url"`
+	EventType string `json:"event_type"`
+	Title string `json:"title"`
+	Description string `json:"description"`
+	Catch string `json:"catch"`
+	HashTag string `json:"hash_tag"`
+	Accepted int `json:"accepted"`
+	Waiting int `json:"waiting"`
+	Limit int `json:"limit"`
+	URL string `json:"url"`
+
+	OwnerID int `json:"owner_id"`
+	OwnerNickname string `json:"owner_nickname"`
+	OwnerTwitterID string `json:"owner_twitter_id"`
+	OwnerDisplayName string `json:"owner_display_name"`
+
+	UpdatedAt time.Time `json:"updated_at"`
+	StartedAt time.Time `json:"started_at"`
+	EndedAt time.Time `json:"ended_at"`
+
+	Address string `json:"address"`
+	Place string `json:"place"`
+	Lat string `json:"lat"`
+	Lon string `json:"lon"`
+
+	Series Series `json:"series"`
 }
 
 type Series struct {
-	URL string
-	ID int
-	Title string
+	URL string`json:"url"`
+	ID int `json:"id"`
+	Title string `json:"title"`
 }
