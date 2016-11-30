ee7dab2c273574e7802a191c3691fbcdc47cddc2
pokohide <hyde31415@gmail.com>
2016-11-30 15:15:45 +0900

    config設定

--- server/config/config.go
+++ server/config/config.go
@@ -1,38 +1,176 @@
 package config
 
 import (
-	"github.com/spf13/viper"
-	"log"
-	"path/filepath"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "os"
+    "reflect"
+    "strings"
+
+    "github.com/OthloTech/OthloCalendar/server/misc"
 )
 
-var config *viper.Viper
-
-func Init(env string) {
-	var err error
-	v := viper.New()
-	v.SetConfigType("yaml")
-	v.SetConfigName(env)
-	if env == "test" {
-		v.AddConfigPath("../config/")
-	} else {
-		v.AddConfigPath("config/")
-	}
-	err = v.ReadInConfig()
-
-	if err != nil {
-		log.Fatal("error on parsing configration file")
-	}
-	config = v
+func defaultConfig() Config {
+    gopath := os.Getenv("GOPATH")
+    return Config{
+        Name:           "GoMicroservices",
+        Port:           80,
+        LogLevel:       4,
+        Mode:           "production",
+        StaticFileHost: "",
+        StaticFilePath: gopath + "/src/github.com/pottava/golang-microservices/app-webui/app",
+        AccessLog:      true,
+        SecuredCookie:  false,
+    }
 }
 
-func relativePath(basedir string, path *string) {
-	p := *path
-	if p != "" && p[0] != '/' {
-		*path = filepath.Join(basedir, p)
-	}
+// NewConfig returns a config struct created by
+// merging environment variables and a config file.
+func NewConfig() *Config {
+    temp := environmentConfig()
+    config := &temp
+
+    if !config.complete() {
+        config.merge(fileConfig())
+    }
+    defer func() {
+        config.merge(defaultConfig())
+        config.trimWhitespace()
+    }()
+    return config
 }
 
-func GetConfig() *viper.Viper {
-	return config
+func environmentConfig() Config {
+    return Config{
+        Name:           os.Getenv("APP_NAME"),
+        Port:           misc.ParseUint16(os.Getenv("APP_PORT")),
+        LogLevel:       misc.Atoi(os.Getenv("APP_LOG_LEVEL")),
+        Mode:           os.Getenv("APP_MODE"),
+        StaticFileHost: os.Getenv("APP_STATIC_FILE_HOST"),
+        StaticFilePath: os.Getenv("APP_STATIC_FILE_PATH"),
+        AccessLog:      misc.ParseBool(os.Getenv("APP_ACCESS_LOG")),
+        SecuredCookie:  misc.ParseBool(os.Getenv("APP_SECURED_COOKIE")),
+    }
 }
+
+func toStringArray(values string) []string {
+    if misc.ZeroOrNil(values) {
+        return []string{}
+    }
+    return strings.Split(values, ",")
+}
+
+func fileConfig() Config {
+    path := misc.NVL(os.Getenv("CONFIG_FILE_PATH"), "/etc/golang-microservices/config.json")
+    file, err := os.Open(path)
+    if err != nil {
+        return Config{}
+    }
+    data, err := ioutil.ReadAll(file)
+    if err != nil {
+        log.Fatal("Unable to read config file", "err:", err)
+        return Config{}
+    }
+    if strings.TrimSpace(string(data)) == "" {
+        return Config{}
+    }
+    config := Config{}
+    err = json.Unmarshal(data, &config)
+    if err != nil {
+        log.Fatal("Error reading config json data. [message] ", err)
+    }
+    return config
+}
+
+func (config *Config) merge(arg Config) *Config {
+    mine := reflect.ValueOf(config).Elem()
+    theirs := reflect.ValueOf(&arg).Elem()
+
+    for i := 0; i < mine.NumField(); i++ {
+        myField := mine.Field(i)
+        if misc.ZeroOrNil(myField.Interface()) {
+            myField.Set(reflect.ValueOf(theirs.Field(i).Interface()))
+        }
+    }
+    return config
+}
+
+func (config *Config) complete() bool {
+    cfgs := reflect.ValueOf(config).Elem()
+
+    for i := 0; i < cfgs.NumField(); i++ {
+        if misc.ZeroOrNil(cfgs.Field(i).Interface()) {
+            return false
+        }
+    }
+    return true
+}
+
+func (config *Config) trimWhitespace() {
+    cfgs := reflect.ValueOf(config).Elem()
+    cfgAttrs := reflect.Indirect(reflect.ValueOf(config)).Type()
+
+    for i := 0; i < cfgs.NumField(); i++ {
+        field := cfgs.Field(i)
+        if !field.CanInterface() {
+            continue
+        }
+        attr := cfgAttrs.Field(i).Tag.Get("trim")
+        if len(attr) == 0 {
+            continue
+        }
+        if field.Kind() != reflect.String {
+            continue
+        }
+        str := field.Interface().(string)
+        field.SetString(strings.TrimSpace(str))
+    }
+}
+
+// String returns a string representation of the config.
+func (config *Config) String() string {
+    return fmt.Sprintf(
+        "Name: %v, Port: %v, LogLevel: %v, Mode: %v, StaticFileHost: %v, StaticFilePath: %v, "+
+            "AccessLog: %v, SecuredCookie: %v ",
+        config.Name, config.Port, config.LogLevel, config.Mode, config.StaticFileHost, config.StaticFilePath,
+        config.AccessLog, config.SecuredCookie)
+}
+
+// import (
+// 	"github.com/spf13/viper"
+// 	"log"
+// 	"path/filepath"
+// )
+
+// var config *viper.Viper
+
+// func Init(env string) {
+// 	var err error
+// 	v := viper.New()
+// 	v.SetConfigType("yaml")
+// 	v.SetConfigName(env)
+// 	if env == "test" {
+// 		v.AddConfigPath("../config/")
+// 	} else {
+// 		v.AddConfigPath("config/")
+// 	}
+// 	err = v.ReadInConfig()
+
+// 	if err != nil {
+// 		log.Fatal("error on parsing configration file")
+// 	}
+// 	config = v
+// }
+
+// func relativePath(basedir string, path *string) {
+// 	p := *path
+// 	if p != "" && p[0] != '/' {
+// 		*path = filepath.Join(basedir, p)
+// 	}
+// }
+
+// func GetConfig() *viper.Viper {
+// 	return config
+// }
